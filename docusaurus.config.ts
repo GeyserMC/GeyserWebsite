9749175b67bce7e9f25d8b8c8af78a009f70f23f
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2024-07-14 13:51:47 -0700

    Remove locale testing
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
     // may want to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'en',
-        locales: ['en', 'de'],
+        locales: ['en'],
     },
 
     presets: [
