e4ff3443194392f3a95772f10596026ef71a7089
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2025-02-28 11:08:54 -0800

    Initial setup i18n
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- src/components/MDXTranslatable.tsx
+++ src/components/MDXTranslatable.tsx
@@ -1,4 +1,8 @@
-import Translate from "@docusaurus/Translate"
+import Translate, { translate } from "@docusaurus/Translate"
+import Link from '@docusaurus/Link';
+import { InlineCode } from "./InlineCode";
+
+const downloadsPage = <Translate id="mdx.setup.downloads_page">downloads page</Translate>;
 
 export const MDXTranslatable = {
     setup: {
@@ -6,8 +10,67 @@ export const MDXTranslatable = {
         self_host: () => <Translate id="mdx.setup.self_host">Self Host</Translate>,
         paper_spigot: () => <Translate id="mdx.setup.paper_spigot">Paper / Spigot</Translate>,
         modded_servers: () => <Translate id="mdx.setup.modded_servers">Modded Servers</Translate>,
+        fabric: () => <Translate id="mdx.setup.fabric">Fabric</Translate>,
+        neoforge: () => <Translate id="mdx.setup.neoforge">NeoForge</Translate>,
         proxy_servers: () => <Translate id="mdx.setup.proxy_servers">Proxy Servers</Translate>,
+        velocity: () => <Translate id="mdx.setup.velocity">Velocity</Translate>,
+        bungeecord: () => <Translate id="mdx.setup.bungeecord">BungeeCord</Translate>,
         viaproxy: () => <Translate id="mdx.setup.viaproxy">ViaProxy</Translate>,
         standalone: () => <Translate id="mdx.setup.standalone">Standalone</Translate>,
+
+        installing_geyser: () => <Translate id="mdx.setup.installing_geyser">Installing Geyser</Translate>,
+        download_jar: ({ jarName }: { jarName: string }) => (
+            <Translate id="mdx.setup.download_jar" values={{
+                jarName: <InlineCode>{jarName}</InlineCode>,
+                downloadsPage: <Link to='/download'>{downloadsPage}</Link>
+            }}>
+                {"Download {jarName} from the {downloadsPage}."}
+            </Translate>
+        ),
+        place_jar: ({ jarName, jarDirectory }: { jarName: string, jarDirectory: string }) => (
+            <Translate id="mdx.setup.place_jar" values={{
+                jarName: <InlineCode>{jarName}</InlineCode>,
+                jarDirectory: <InlineCode>{jarDirectory}</InlineCode>
+            }}>
+                {"Place the {jarName} in the {jarDirectory} folder."}
+            </Translate>
+        ),
+        fabric_dependency: () => (
+            <Translate id="mdx.setup.fabric_dependency" values={{
+                fabricApiLink: <Link to='https://modrinth.com/mod/fabric-api'>FabricAPI</Link>,
+            }}>
+                {"If it is not already installed, download  and install the {fabricApiLink} mod to the same folder."}
+            </Translate>
+        ),
+        restart_server: () => <Translate id="mdx.setup.restart_server">Restart the server.</Translate>,
+
+        geyser_config_changes: () => <Translate id="mdx.setup.geyser_config_changes">Geyser Config Changes</Translate>,
+        open_config: ({ configLocation }: { configLocation: string }) => (
+            <Translate id="mdx.setup.open_config" values={{ configLocation: <InlineCode>{configLocation}</InlineCode> }}>
+                {"Open your Geyser config located at {configLocation} and make these changes:"}
+            </Translate>
+        ),
+        change_address: ({ address }: { address: string }) => (
+            <Translate id="mdx.setup.change_address" values={{ address: <InlineCode>{address}</InlineCode>, addressText: <InlineCode>address</InlineCode> }}>
+                {"Change the {addressText} value to {address}."}
+            </Translate>
+        ),
+        change_port: ({ port }: { port: string }) => (
+            <Translate id="mdx.setup.change_port" values={{ port: <InlineCode>{port}</InlineCode>, portText: <InlineCode>port</InlineCode> }}>
+                {"Change the {portText} value to {port}."}
+            </Translate>
+        ),
+        change_clone_remote_port: ({ port }: { port: string }) => (
+            <Translate id="mdx.setup.change_clone_remote_port" values={{ port: <InlineCode>{port}</InlineCode>, portText: <InlineCode>clone-remote-port</InlineCode> }}>
+                {"Change the {portText} value to {port}."}
+            </Translate>
+        ),
+
+        additional_steps: () => <Translate id="mdx.setup.additional_steps">Additional Steps</Translate>,
+        connecting_on_bedrock: () => <Translate id="mdx.setup.connecting_on_bedrock">Connecting on Bedrock</Translate>,
+        more_information: () => <Translate id="mdx.setup.more_information">More Information</Translate>,
+
+        hosting_provider_article: () => <Translate id="mdx.setup.hosting_provider_article">Hosting provider article</Translate>,
+        hosting_provider_support: () => <Translate id="mdx.setup.hosting_provider_support">Hosting provider support</Translate>,
     }
 }
--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,8 +1,12 @@
 import type { HostingProvider, ProviderType } from "@site/src/types/providers";
-import ReactMarkdown from "react-markdown";
 import React, { useState } from 'react';
 import { translate } from "@docusaurus/Translate";
 import { providersData } from "../data/providers";
+import Admonition from '@theme/Admonition';
+import { MDXTranslatable } from "./MDXTranslatable";
+import ReactMarkdown from 'react-markdown';
+import Link from '@docusaurus/Link';
+import Heading from '@theme/Heading';
 
 export const noP = (props: { children: any; }) => {
     const { children } = props;
@@ -17,21 +21,16 @@ export const Provider = ({ type }) => {
             <ul>{hostingProviders.map((provider: HostingProvider) => (
                 <li>
                     <a href={provider.url}>{provider.name}</a>
-                    {provider.description != null ? (
-                        <ReactMarkdown children={`&nbsp;&hyphen; ${provider.description}`} components={{ p: noP }} />
-                    ) : (
-                        ''
-                    )}
                 </li>
             ))}</ul>
         </div>
     )
 }
 
-export const ProviderSelector = ({setProvider}) => {
+export const ProviderSelector = ({ setProvider }) => {
     const providers: HostingProvider[] = [
         ...Object.values(providersData.built_in),
-        ...Object.values(providersData.support), 
+        ...Object.values(providersData.support),
         ...Object.values(providersData.no_support)
     ].flat().sort((a, b) => a.name.localeCompare(b.name));
 
@@ -50,7 +49,7 @@ export const ProviderSelector = ({setProvider}) => {
             message: "If these instructions do not work, contact your server hosting provider and ask for a UDP port. Then, set clone-remote-port to false, and set 'bedrock port' to the port you got. For VPS/KVM servers, please follow the self-hosting steps."
         })
     } as HostingProvider);
-    
+
 
     const [selectedProvider, setSelectedProvider] = useState(null);
 
@@ -73,4 +72,184 @@ export const ProviderSelector = ({setProvider}) => {
             </select>
         </div>
     );
+}
+
+interface ProviderGeyserSetupProps {
+    provider: {
+        warn?: string;
+        custom_install_location?: string;
+        config?: {
+            address?: string;
+            port?: string;
+            clone_remote_port?: string;
+            port_instruction?: string;
+            address_instruction: string;
+            other_instructions?: string;
+        }
+        additional_step?: string;
+        connect_instructions?: string;
+        info?: string;
+        hosting_article?: string;
+        hosting_support?: string;
+    };
+    jarName: string;
+    jarDirectory: string;
+    configLocation: string;
+    dependencyTranslation?: () => React.JSX.Element;
+}
+
+export const ProviderGeyserSetup: React.FC<ProviderGeyserSetupProps> = ({ provider, jarName, jarDirectory, configLocation, dependencyTranslation }) => {
+    return (
+        <>
+            { provider && (
+                <>
+                    {provider.warn && (
+                        <Admonition type="warning">
+                            <ReactMarkdown>
+                                {provider.warn}
+                            </ReactMarkdown>
+                        </Admonition>
+                    )}
+
+                    {provider.custom_install_location ? (
+                        <>
+                            <Heading as="h2" id="installing-geyser">
+                                <MDXTranslatable.setup.installing_geyser />
+                            </Heading>
+                            <ReactMarkdown>
+                                {provider.custom_install_location}
+                            </ReactMarkdown>
+                        </>
+                    ) : (
+                        <>
+                            <Heading as="h2" id="installing-geyser">
+                                <MDXTranslatable.setup.installing_geyser />
+                            </Heading>
+                            <ol>
+                                <li>
+                                    <MDXTranslatable.setup.download_jar jarName={jarName} />
+                                </li>
+                                <li>
+                                    <MDXTranslatable.setup.place_jar jarName={jarName} jarDirectory={jarDirectory}/>
+                                </li>
+                                {dependencyTranslation && (
+                                    <li>
+                                        {dependencyTranslation()}
+                                    </li>
+                                )}
+                                <li>
+                                    <MDXTranslatable.setup.restart_server />
+                                </li>
+                            </ol>
+                        </>
+                    )}
+
+                    {provider.config && (
+                        <>
+                            <Heading as="h3" id="geyser-config-changes">
+                                <MDXTranslatable.setup.geyser_config_changes />
+                            </Heading>
+                            <MDXTranslatable.setup.open_config configLocation={configLocation} />
+                            <ul>
+                                {provider.config.address && (
+                                    <li>
+                                        <MDXTranslatable.setup.change_address address={provider.config.address} />
+                                    </li>
+                                )}
+                                {provider.config.port && (
+                                    <li>
+                                        <MDXTranslatable.setup.change_port port={provider.config.port} />
+                                    </li>
+                                )}
+                                {provider.config.clone_remote_port && (
+                                    <li>
+                                        <MDXTranslatable.setup.change_clone_remote_port port={'true'} />
+                                    </li>
+                                )}
+                                {provider.config.port_instruction && (
+                                    <li>
+                                        <ReactMarkdown>
+                                            {provider.config.port_instruction}
+                                        </ReactMarkdown>
+                                    </li>
+                                )}
+                                {provider.config.address_instruction && (
+                                    <li>
+                                        <ReactMarkdown>
+                                            {provider.config.address_instruction}
+                                        </ReactMarkdown>
+                                    </li>
+                                )}
+                                {provider.config.other_instructions && (
+                                    <li>
+                                        <ReactMarkdown>
+                                            {provider.config.other_instructions}
+                                        </ReactMarkdown>
+                                    </li>
+                                )}
+                            </ul>
+                        </>
+                    )}
+
+                    {provider.additional_step && (
+                        <>
+                            <Heading as="h3" id="additional-steps">
+                                <MDXTranslatable.setup.additional_steps />
+                            </Heading>
+                            <ReactMarkdown>
+                                {provider.additional_step}
+                            </ReactMarkdown>
+                        </>
+                    )}
+
+
+                    {provider.connect_instructions && (
+                        <>
+                            <Heading as="h3" id="connecting-on-bedrock">
+                                <MDXTranslatable.setup.connecting_on_bedrock />
+                            </Heading>
+                            <ReactMarkdown>
+                                {provider.connect_instructions}
+                            </ReactMarkdown>
+                        </>
+                    )}
+
+                    {provider.info && (
+                        <>
+                            <Admonition type="info">
+                                <ReactMarkdown>
+                                    {provider.info}
+                                </ReactMarkdown>
+                            </Admonition>
+                        </>
+                    )}
+
+
+                    {(provider.hosting_article || provider.hosting_support) && (
+                        <>
+                            <Heading as="h3" id="more-information">
+                                <MDXTranslatable.setup.more_information />
+                            </Heading>
+                            <ul>
+                                {provider.hosting_article && (
+                                    <li>
+                                        <Link to={provider.hosting_article}>
+                                            <MDXTranslatable.setup.hosting_provider_article />
+                                        </Link>
+                                    </li>
+                                )}
+                                {provider.hosting_support && (
+                                    <li>
+                                        <Link to={provider.hosting_support}>
+                                            <MDXTranslatable.setup.hosting_provider_support />
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        </>
+                    )}
+                </>
+            )}
+        </>
+    )
 }
